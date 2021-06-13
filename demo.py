22bd78662b40f029b7a167483740b4eba38a3165
RTT-ThinkPad <renantaylan@gmail.com>
2021-06-13 20:58:59 +0300

    getSelectable

--- demo.py
+++ demo.py
@@ -1,3 +1,6 @@
+import numpy as np
+import random
+
 class Node:
     def __init__(self,value) -> None:
         self.value = value
@@ -36,3 +39,52 @@ class Node:
             return repr(str(self.value[1]))
         elif not self.isOpen:
             return repr("X")
+
+class Table:
+    def __init__(self) -> None:
+        suits = ['Clubs', 'Diamonds', 'Spikes', 'Hearts']
+        values = np.arange(1,14).tolist()
+
+        deck = []
+
+        for s in suits:
+            for v in values:
+                deck.append([s,v])
+        
+        random.shuffle(deck)
+        
+        self.deck = deck
+
+    def generateTable(self):
+        self.table = dict()
+        for j in range(1,8):
+            for i in range(1,j+1):
+                n = Node(self.deck.pop())
+                if j != 7:
+                    n.left = (j+1) * 10 + i
+                    n.right = (j+1) * 10 + (i+1)
+                else:
+                    pass
+                    #n.isOpen = True
+                self.table[j*10 + i] = n
+        self.updateTable()
+    
+    def updateTable(self):
+        for k in self.table.keys():
+            self.table[k].update(self.table, self.deck[-1][1])
+
+    def succes(self):
+        for c in self.table.keys():
+            if self.table[c].isDestroyed == False:
+                return False
+        print('Solved!!')
+        return True
+
+    def getSelectable(self):
+        selectableList = []
+        for k in self.table:
+            if self.table[k].isSelectable:
+                selectableList.append(k)
+        return selectableList
+        
+
